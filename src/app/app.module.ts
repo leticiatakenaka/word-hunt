6d180559962431a911490523fe6ce5c8762575de
Leticia Takenaka <leticiatakenaka12@gmail.com>
2023-07-23 17:23:07 -0300

    teclado virtual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { VirtualKeyboardComponent } from './virtual-keyboard/virtual-keyboard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, VirtualKeyboardComponent],
+  imports: [BrowserModule, AppRoutingModule, MatToolbarModule, FormsModule, MatFormFieldModule, MatInputModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
