38d4e3548e6f42a4d80461a36bdec3e5abeb2710
Leticia Takenaka <leticiatakenaka12@gmail.com>
2023-07-23 21:19:40 -0300

    mover clique para virtual keyboard

--- src/app/display-word/display-word.component.ts
+++ src/app/display-word/display-word.component.ts
@@ -13,30 +13,29 @@ export class DisplayWordComponent implements OnInit {
     private dictionaryService: DictionaryService
   ) { }
 
-  word!: String;
+  word!: string;
   word_letters!: string[];
-  definition!: String;
+  definition!: string;
 
   ngOnInit() {
-    // this.word = "convince";
-    // this.word_letters = this.word.toUpperCase().split('');
-    // this.definition = "To make someone believe, or feel sure about something, especially by using logic, argument or evidence.";
-    this.randomWordsService.getWord().subscribe((data) => {
-      this.dictionaryService
-        .getWordDefinition(data[0].word)
-        .subscribe((data) => {
-          for (let i = 0; i < data[0].meanings.length; i++) {
-            if (data[0].meanings[i].partOfSpeech === 'verb') {
-              console.log(data[0].meanings[i].definitions[0]);
-              this.definition = data[0].meanings[i].definitions[0].definition;
-              this.word = data[0].word;
-              console.log(this.word);
-              this.word_letters = this.word.toUpperCase().split('');
-              break;
-            }
-          }
-        });
-    });
-
+    this.word = "convince";
+    this.word_letters = this.word.toUpperCase().split('');
+    this.definition = "To make someone believe, or feel sure about something, especially by using logic, argument or evidence.";
+    // this.randomWordsService.getWord().subscribe((data) => {
+    //   this.dictionaryService
+    //     .getWordDefinition(data[0].word)
+    //     .subscribe((data) => {
+    //       for (let i = 0; i < data[0].meanings.length; i++) {
+    //         if (data[0].meanings[i].partOfSpeech === 'verb') {
+    //           console.log(data[0].meanings[i].definitions[0]);
+    //           this.definition = data[0].meanings[i].definitions[0].definition;
+    //           this.word = data[0].word;
+    //           console.log(this.word);
+    //           this.word_letters = this.word.toUpperCase().split('');
+    //           break;
+    //         }
+    //       }
+    //     });
+    // });
   }
 }
--- src/app/virtual-keyboard/virtual-keyboard.component.ts
+++ src/app/virtual-keyboard/virtual-keyboard.component.ts
@@ -14,5 +14,6 @@ export class VirtualKeyboardComponent {
 
   onKeyPress(key: string): void {
     this.keyPress.emit(key);
+    console.log(key);
   }
 }
