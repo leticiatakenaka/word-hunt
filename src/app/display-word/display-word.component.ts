58a6777723076b655643b3a69153a8205e2ac536
Leticia Takenaka <leticiatakenaka12@gmail.com>
2023-07-23 21:40:37 -0300

    separar definicao e palavra do componente display

--- src/app/display-word/display-word.component.ts
+++ src/app/display-word/display-word.component.ts
@@ -13,24 +13,21 @@ export class DisplayWordComponent implements OnInit {
     private dictionaryService: DictionaryService
   ) { }
 
-  word!: string;
-  word_letters!: string[];
-  definition!: string;
+  word: string = 'convince';
+  definition: string = 'To make someone believe, or feel sure about something, especially by using logic, argument or evidence';
+  word_letters: string[] = this.word.toUpperCase().split('');
 
   ngOnInit() {
-    this.word = "convince";
-    this.word_letters = this.word.toUpperCase().split('');
-    this.definition = "To make someone believe, or feel sure about something, especially by using logic, argument or evidence.";
     // this.randomWordsService.getWord().subscribe((data) => {
     //   this.dictionaryService
     //     .getWordDefinition(data[0].word)
     //     .subscribe((data) => {
     //       for (let i = 0; i < data[0].meanings.length; i++) {
     //         if (data[0].meanings[i].partOfSpeech === 'verb') {
-    //           console.log(data[0].meanings[i].definitions[0]);
-    //           this.definition = data[0].meanings[i].definitions[0].definition;
-    //           this.word = data[0].word;
-    //           console.log(this.word);
+    //           const word = data[0].word;
+    //           const definition = data[0].meanings[i].definitions[0].definition;
+    //           this.word = word;
+    //           this.randomWordsService.setWordAndDefinition(word, definition);
     //           this.word_letters = this.word.toUpperCase().split('');
     //           break;
     //         }
